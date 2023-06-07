99142e722f8790a457b0a751bf8888fbd8549b28
MonikaValecha <129657953+MonikaValecha@users.noreply.github.com>
2023-06-07 10:40:09 +0530

    Update ReportHeader.jsx

--- src/components/ReportHeader.jsx
+++ src/components/ReportHeader.jsx
@@ -4,8 +4,8 @@ function ReportHeader({ StoreArray }) {
   return (
     <tbody>
       <tr>
-        <th className="fixed-column">S.No</th>
-        <th className="fixed-column">Issues</th>
+        <th>S.No</th>
+        <th>Issues</th>
         {StoreArray.map((data, index) => {
           return <th key={index}>{data}</th>;
         })}
